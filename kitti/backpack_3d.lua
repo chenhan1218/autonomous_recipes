dd5441194338c0738c3611438af1ac19005011fc
Chen-Han Hsiao (Stanley) <chenhan.hsiao.tw@gmail.com>
2017-10-09 18:50:48 +0800

    adjust parameter

--- kitti/backpack_3d.lua
+++ kitti/backpack_3d.lua
@@ -37,9 +37,11 @@ options = {
 MAP_BUILDER.use_trajectory_builder_3d = true
 MAP_BUILDER.num_background_threads = 7
 SPARSE_POSE_GRAPH.optimization_problem.huber_scale = 5e2
-SPARSE_POSE_GRAPH.optimize_every_n_scans = 320
+SPARSE_POSE_GRAPH.optimize_every_n_scans = 2
 SPARSE_POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
 SPARSE_POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
 SPARSE_POSE_GRAPH.constraint_builder.min_score = 0.62
+TRAJECTORY_BUILDER_3D.motion_filter.max_distance_meters = 2
+TRAJECTORY_BUILDER_3D.motion_filter.max_angle_radians = 0.04
 
 return options
